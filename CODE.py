eb8a171f514dba985db830c1f6d6bf964c3ccc56
Chidera0001 <c.anele@alustudent.com>
2023-10-03 21:44:10 +0200

    Task

--- CODE.py
+++ CODE.py
@@ -1,70 +1,65 @@
 import re
 
-# API responses
-responses = [
-    "Restaurant Name - Cuisine Type",
-    "ingredient1, ingredient2, ingredient3",
-    "rgb(255, 255, 255)",
-    "Check out my profile @username",
-    "Here's a product code: ABC123",
-    "Headline: Subheader",
-    "Oct 15, 2023 - 08:30 AM",
-    "Contact us at email@example.com",
-]
+# Sample input string containing various data types
+input_string = """
+Restaurant ABC - Italian, Restaurant XYZ - Mexican, Restaurant DEF - Chinese
+Ingredients: ingredient1, ingredient2, ingredient3, ingredient4
+This is an RGB color: rgb(255, 0, 128)
+Check out my profile: @username123 on Twitter
+Product Codes: ABC123, XYZ456, DEF789
+News Headlines: Breaking News: Important Update
+Event Date: Sep 30, 2023 - 02:30 PM
+Email Address: sample.email@example.com
+"""
 
-# Extracting Restaurant Names and Cuisine Types
-for response in responses:
-    match = re.search(r"([\w\s]+) - ([\w\s]+)", response)
-    if match:
-        restaurant_name = match.group(1)
-        cuisine_type = match.group(2)
-        print(f"Restaurant Name: {restaurant_name}, Cuisine Type: {cuisine_type}")
+# Regular expressions for data extraction
+restaurant_pattern = r"([A-Za-z\s]+) - ([A-Za-z\s]+)"
+ingredient_pattern = r"Ingredients:\s*(.*)"
+rgb_pattern = r"rgb\((\d+),\s*(\d+),\s*(\d+)\)"
+username_pattern = r"@\w+"
+product_code_pattern = r"[A-Z]{3}\d{3}"
+news_headline_pattern = r"([^:]+):\s*(.*)"
+event_datetime_pattern = r"(\w+\s\d{2},\s\d{4}) - (\d{2}:\d{2}\s[APap][Mm])"
+email_pattern = r"[\w\.-]+@[\w\.-]+"
 
-# Extracting Event Dates and Times
-for response in responses:
-    match = re.search(r"(\w{3} \d{2}, \d{4} - \d{2}:\d{2} [APap][Mm])", response)
-    if match:
-        event_datetime = match.group(0)
-        print(f"Event Date/Time: {event_datetime}")
+# Extract restaurant names and cuisines
+restaurant_matches = re.findall(restaurant_pattern, input_string)
+for match in restaurant_matches:
+    restaurant_name, cuisine = match
+    print("Restaurant Name: {}, Cuisine: {}".format(restaurant_name, cuisine))
 
-# Extracting RGB Color Values
-for response in responses:
-    match = re.search(r"rgb\((\d+),\s*(\d+),\s*(\d+)\)", response)
-    if match:
+# Extract ingredient lists
+ingredient_matches = re.search(ingredient_pattern, input_string)
+if ingredient_matches:
+    ingredients = ingredient_matches.group(1).split(', ')
+    print("Ingredients:", ', '.join(ingredients))
 
-# Extracting Event Dates and Times
-for response in responses:
-    match = re.search(r"(\w{3} \d{2}, \d{4} - \d{2}:\d{2} [APap][Mm])", response)
-    if match:
-        event_datetime = match.group(0)
-        print(f"Event Date/Time: {event_datetime}")
-        r = match.group(1)
-        g = match.group(2)
-        b = match.group(3)
-        print(f"RGB Color: ({r}, {g}, {b})")   
+# Extract RGB color values
+rgb_matches = re.search(rgb_pattern, input_string)
+if rgb_matches:
+    red, green, blue = rgb_matches.groups()
+    print("Red: {}, Green: {}, Blue: {}".format(red, green, blue))
 
-# Extracting Social Media Usernames
-for response in responses:
-    usernames = re.findall(r"@(\w+)", response)
-    if usernames:
-       print(f"Username: {', '.join(usernames)}")
+# Extract social media usernames
+username_matches = re.findall(username_pattern, input_string)
+print("Usernames:", ', '.join(username_matches))
 
-     # Extracting Product Codes
-for response in responses:
-    match = re.search(r"([A-Z]{3}\d{3})", response)
-    if match:
-        product_code = match.group(1)
-        print(f"Product Code: {product_code}")
+# Extract product codes
+product_code_matches = re.findall(product_code_pattern, input_string)
+print("Product Codes:", ', '.join(product_code_matches))
 
-# Extracting Ingredient Lists
-for response in responses:
-    ingredients = re.findall(r"([^,]+)(?:,\s*([^,]+))*", response)
-    if ingredients:
-        print(f"Ingredients: {', '.join(ingredients)}")
+# Extract news headlines
+news_headline_matches = re.findall(news_headline_pattern, input_string)
+for match in news_headline_matches:
+    headline, subheader = match
+    print("Headline: {}, Subheader: {}".format(headline, subheader))
 
-# Extracting Email Addresses
-for response in responses:
-    email = re.search(r"([\w.-]+@[\w.-]+)", response)
-    if email:
-        email_address = email.group(1)
-        print(f"Email Address: {email_address}")
+# Extract event dates and times
+event_datetime_matches = re.search(event_datetime_pattern, input_string)
+if event_datetime_matches:
+    event_date, event_time = event_datetime_matches.groups()
+    print("Event Date: {}, Event Time: {}".format(event_date, event_time))
+
+# Extract email addresses
+email_matches = re.findall(email_pattern, input_string)
+print("Email Addresses:", ', '.join(email_matches))
